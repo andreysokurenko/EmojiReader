1793c510ec0275377881a82dcf1bb07c07477fca
andreysokurenko <andreysokurenko2@gmail.com>
2020-03-31 22:58:29 +0300

    Initial Commit

---
+++ EmojiReader/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EmojiReader
+//
+//  Created by Andrey on 31.03.2020.
+//  Copyright © 2020 Andrey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
